223769a4b680e95e1f4a5161914a90f216f8ecee
Nhat <mrdavipnhat123@gmail.com>
2025-10-28 18:59:58 +1100

    feat: add home route

--- components/navigation-header.tsx
+++ components/navigation-header.tsx
@@ -9,20 +9,21 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 const navigationItems = [
-  "About us",
-  "Our Solutions", 
-  "Why Us",
-  "Facility",
-  "Meet Our Team",
-  "Our Products",
-  "Our Process",
-  "Contact Us"
+   "Home",
+   "About us",
+   "Our Solutions", 
+   "Why Us",
+   "Facility",
+   "Meet Our Team",
+   "Our Products",
+   "Our Process",
+   "Contact Us"
 ];
 
 // Menu icon component
 const MenuIcon = () => (
    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-   <path strokeLinecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+   <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
    </svg>
 );
 
